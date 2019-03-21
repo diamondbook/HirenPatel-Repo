a5c43781e8a7646f2df1699f00afbec7e64b5839
erhir <erhir@LAPTOP-K9GQO8KP>
2019-03-21 11:01:09 +0530

    My first commit

---
+++ TestProject/src/com/Hello.java
@@ -0,0 +1,10 @@
+package com;
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first JAVA code after 10 Years !!!");
+		System.out.println("Good JOB");
+	}
+
+}
